3f00e2ec3c6c937b1ec3b0013922800304d906a8
raidas <mrraidas@gmail.com>
2022-12-07 17:35:37 +0200

    update

--- src/getAccountData.js
+++ src/getAccountData.js
@@ -1,29 +1,35 @@
 
-const url = 'https://proxy.berkeley.minaexplorer.com/graphql'
+const url = 'https://berkeley.graphql.minaexplorer.com/'
 // const url = 'https://graphql.minaexplorer.com/'
 const publicKey = 'B62qr8tFfnB7FANuJYnfwvffh8ERr2Nv8MZPZtER8NgJ811uJvQrSGM'
 // const publicKey = 'B62qnuJGE11DiYL1WJm1UQafjMvqinEZfXK2i2K9TNwMY27iKTxmGWd'
 
-fetch(url, {
+const response = await fetch(url, {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
   },
   body: JSON.stringify({
     query: `
-      query MyQuery {
-        transactions(limit: 5, sortBy: DATETIME_DESC, query: {from: \"${publicKey}"\}) {
-          from
-          to
-          nonce
-          amount
-          hash
+    query MyQuery {
+      zkapps(query: {zkappCommand: {accountUpdates: {body: {publicKey: \"${publicKey}"\}}}}) {
+        hash
+        blockHeight
+        dateTime
+        zkappCommand {
+          feePayer {
+            body {
+              publicKey
+            }
+          }
         }
       }
+    }
+
       `
   }),
 })
-  .then((res) => res.json())
-  .then((result) => console.log(result));
+const response_ = await response.json()
+console.log(JSON.stringify(response_, null, 2))
 
 export {}
